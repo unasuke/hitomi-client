6d1a8d75303eb7f0388397fa5f4ab1fdb382a780
Yusuke Nakamura <yusuke1994525@gmail.com>
2017-06-23 00:09:07 +0900

    Send visit page data to api

--- src/background.js
+++ src/background.js
@@ -1,5 +1,14 @@
 chrome.runtime.onMessage.addListener(receiveMessage)
 
 function receiveMessage(request, sender, sendResponse) {
-  console.log(request)
+  var form = new FormData();
+  form.append("history[title]", request["title"])
+  form.append("history[url]", request["url"])
+  form.append("history[raw_user_agent]", navigator.userAgent)
+  fetch("http://localhost:4000/histories", {
+    method: "POST",
+    body: form
+  }).then((response) => {
+    console.log(response)
+  })
 }
