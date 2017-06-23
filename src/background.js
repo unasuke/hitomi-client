f10231b2bd2bc789abc1d4c169acb664b7b79851
Yusuke Nakamura <yusuke1994525@gmail.com>
2017-06-23 18:19:30 +0900

    Follow lint rule

--- src/background.js
+++ src/background.js
@@ -1,14 +1,12 @@
-chrome.runtime.onMessage.addListener(receiveMessage)
-
-function receiveMessage(request, sender, sendResponse) {
-  var form = new FormData();
-  form.append("history[title]", request["title"])
-  form.append("history[url]", request["url"])
-  form.append("history[raw_user_agent]", navigator.userAgent)
-  fetch("http://localhost:4000/histories", {
-    method: "POST",
-    body: form
-  }).then((response) => {
-    console.log(response)
-  })
+function receiveMessage(request) {
+  const form = new FormData();
+  form.append('history[title]', request.title);
+  form.append('history[url]', request.url);
+  form.append('history[raw_user_agent]', navigator.userAgent);
+  fetch('http://localhost:4000/histories', {
+    method: 'POST',
+    body: form,
+  }).then(() => {});
 }
+
+chrome.runtime.onMessage.addListener(receiveMessage);
