83be72a600fb17aa01da53e657112d960ef737cf
Yusuke Nakamura <yusuke1994525@gmail.com>
2017-06-26 23:08:00 +0900

    Use babel

--- webpack.config.js
+++ webpack.config.js
@@ -9,4 +9,15 @@ module.exports = {
     filename: '[name].js',
     path: path.resolve(__dirname, 'dist'),
   },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+    ],
+  },
 };
