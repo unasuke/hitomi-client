476a11a396f234fb0c3865ca6cc1e6bf49fde788
Yusuke Nakamura <yusuke1994525@gmail.com>
2017-06-29 22:31:57 +0900

    Use es module and refactor

--- src/options.js
+++ src/options.js
@@ -1,57 +1,23 @@
-class Options {
+export default class Options {
   constructor() {
-    chrome.storage.sync.get('hitomi-client-server-address', (value) => {
-      //console.dir(value['hitomi-client-server-address']);
-      this.serverAddress = value['hitomi-client-server-address'];
-    });
-    chrome.storage.sync.get('hitomi-client-server-secret', (value) => {
-      //console.log(value);
-      this.serverSecret = value['hitomi-client-server-secret'];
-    });
-    //sleep(5);
+    this.data = {
+      server: {
+        address: '',
+        secret: '',
+      },
+    };
   }
 
-  // setServerAddress(serverAddress) {
-  //   this.serverAddress = serverAddress;
-  // }
-
-  // getServerAddress() {
-  //   return this.serverAddress;
-  // }
-
-  // setServerSecret(serverSecret) {
-  //   this.serverAddress = serverSecret;
-  // }
-
-  syncStorage() {
-    chrome.storage.sync.set({ 'hitomi-client-server-address': this.serverAddress });
-    chrome.storage.sync.set({ 'hitomi-client-server-secret': this.serverSecret });
+  fetchStorage(callback) {
+    chrome.storage.sync.get(null, (value) => {
+      this.data = value.data;
+      callback();
+    });
   }
 
-  asyncSyncStorage() {
-    return new Promise((resolve) => {
-      window.setTimeout(() => {
-        resolve(this.syncStorage());
-      }, 2000);
+  syncStorage(callback) {
+    chrome.storage.sync.set({ data: this.data }, () => {
+      callback();
     });
   }
 }
-
-const options = new Options();
-
-document.addEventListener('DOMContentLoaded', () => {
-  //const options = new Options();
-  const form = document.forms.options;
-
-  form.serverAddress.value = options.serverAddress;
-  form.serverSecret.value = options.serverSecret;
-
-  form.addEventListener('keyup', () => {
-    options.serverAddress = form.serverAddress.value;
-    options.serverSecret = form.serverSecret.value;
-    options.asyncSyncStorage().then(()=>{
-      form.serverAddress.value = options.serverAddress;
-      form.serverSecret.value = options.serverSecret;
-    });
-  });
-});
