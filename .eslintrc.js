2d83c6963d0d1d833d98963261778a9e3395b91e
Yusuke Nakamura <yusuke1994525@gmail.com>
2017-07-16 18:41:21 +0900

    Update eslint settings for jest

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,10 +2,12 @@ module.exports = {
   "env": {
     "browser": true,
     "webextensions": true,
-    "es6": true
+    "es6": true,
+    "jest/globals": true
   },
   "extends": "airbnb-base",
   "plugins": [
-    "import"
+    "import",
+    "jest"
   ]
 };
