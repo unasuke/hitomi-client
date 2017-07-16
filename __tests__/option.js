8676ae952e31a98454cb80a681a6c7d2b4d16940
Yusuke Nakamura <yusuke1994525@gmail.com>
2017-07-16 18:42:01 +0900

    More test for option

--- __tests__/option.js
+++ __tests__/option.js
@@ -4,4 +4,20 @@ test('Create empty option object', () => {
   const option = new Option();
   expect(option.data.server.address).toBe('');
   expect(option.data.server.secret).toBe('');
-})
+});
+
+test('Option.syncStorage()', () => {
+  const option = new Option();
+  const callback = jest.fn();
+
+  option.syncStorage(callback);
+  expect(callback).toHaveBeenCalled();
+});
+
+test('Option.fetchStorage()', () => {
+  const option = new Option();
+  const callback = jest.fn();
+
+  option.fetchStorage(callback);
+  expect(callback).toHaveBeenCalled();
+});
